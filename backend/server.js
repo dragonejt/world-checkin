a259af66365db9c64c3d07a2e4614981efa7acb3
Blugil <dagnello@asu.edu>
2020-10-10 23:24:53 -0700

    fix database connect string, fix schema, add database functionality to express api for /pop and /checkin

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const PopulationModel = require('./PopulationModel');
+const e = require('express');
 require('dotenv').config({path:'../.env'});
 
 const app = express();
@@ -13,12 +15,17 @@ app.use(helmet());
 
 
 const uri = process.env.URI_STRING;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
+mongoose.connect(uri, { 
+  useNewUrlParser: true, 
+  useCreateIndex: true, 
+  useUnifiedTopology: true}
 );
-const connection = mongoose.connection;
-connection.once('open', () => {
+
+mongoose.connection.once('connected', () => {
   console.log("MongoDB database connection established successfully");
 })
+
+
 
 app.get('/', (req, res) => {
   res.send("hello world!");
@@ -26,17 +33,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/pop', (req, res) => {
+  
+  
 
   try {
-    let zip = req.query.zip;
+    
   //database call for population
-    let population = 100;
-    const response = {
-      "zip": zip,
-      "pop": population
-    }
-    res.send(response);
-    res.status(200).end();
+    let zip = req.query.zip;
+    PopulationModel.find({zipCode: zip}).then((data) => {
+
+      const response = data[0];
+      console.log(response);
+      res.send(response);
+      res.status(200).end();
+
+    }).catch((error) => {
+      console.error("There was an error");
+      res.status(404).end();
+    })
+
   } catch (error) {
     console.log(error);
     res.status(404).end();
@@ -48,13 +63,62 @@ app.post('/checkin', (req, res) => {
   try {
 
     let zip = req.query.zip;
-    let population = 101;
-    const response = {
-      "zip": zip,
-      "pop": population
-    }
-    res.send(response);
-    res.status(200).end();
+
+    PopulationModel.find({zipCode: zip}).then((data) => {
+
+      let response = data[0];
+      //if the query returns a result
+      if (response) {
+        //increments population by one
+        response.population += 1;
+
+        //creates a new document based on the upadted data
+        const updateZip = new PopulationModel(response);
+        //adds updates the document in the mongo db
+        updateZip.save((error) => {
+          //if there's an error send 404
+          if (error) {
+            console.error(error);
+            res.status(404).end();
+          }
+          //else send response and 200 
+          else {
+            console.log("Successfully checked in");
+            res.send(response);
+            res.status(200).end();
+          }
+        });
+      }
+      //if the query returns nothing
+      else {
+        //set response to queried zip and default population of 1
+        response = {
+          zipCode: zip,
+          population: 1
+        };
+        //create a new document
+        const newZip = new PopulationModel(response);
+
+        //add the new document to the database
+        newZip.save((error) => {
+          //if error send 404
+          if (error) {
+            console.error(error);
+            res.status(404).end();
+          }
+          //else send response and 200
+          else {
+            console.log("Successfully checked in");
+            res.send(response);
+            res.status(200).end();
+          }
+        })
+      }
+
+    }).catch((error) => {
+      console.error(error);
+      res.status(404).end();
+    })
 
   }
   catch (error) {
